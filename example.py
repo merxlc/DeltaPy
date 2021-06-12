9f8c12b66ea52cbe81b94126133f59c93c834b29
merxlc <59421457+merxlc@users.noreply.github.com>
2021-06-12 14:20:39 +0100

    added example program

--- example.py
+++ example.py
@@ -2,15 +2,21 @@ from nodes import *
 from scripts import *
 from tunnel import *
 
-#init(globals())
+class Counter(Script):
+    def start(self):
+        self.i = 0
+    def update(self):
+        self.i += 1
 
-#root.add_node(Empty, 'base')
+class Printme(Script):
+    def update(self):
+        print(self.counter.i)
 
-#root.base.add_script(Printme)
-#root.base.add_script(Counter)
+root.add_node(Empty, 'base')
 
-#root.base['Printme'].set_var('counter', root.base['Counter'])
+root.base.add_script(Printme)
+root.base.add_script(Counter)
 
-root.add_node(World, 'world')
+root.base['Printme'].set_var('counter', root.base['Counter'])
 
 start()
---
+++ main.py
@@ -0,0 +1,16 @@
+from nodes import *
+from scripts import *
+from tunnel import *
+
+#init(globals())
+
+#root.add_node(Empty, 'base')
+
+#root.base.add_script(Printme)
+#root.base.add_script(Counter)
+
+#root.base['Printme'].set_var('counter', root.base['Counter'])
+
+root.add_node(World, 'world')
+
+start()
